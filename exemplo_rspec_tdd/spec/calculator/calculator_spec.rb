b7e6e56fcba5ea1687d0f01673f3045d59dd09cb
Gabriel <bieldeluna@gmail.com>
2023-01-04 23:28:19 -0300

    Continuação matchers ruby on rails

--- exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
+++ exemplo_rspec_tdd/spec/calculator/calculator_spec.rb
@@ -1,8 +1,17 @@
 require 'calculator'
 
-describe Calculator do
+RSpec.describe Calculator, "Sobre a Calculadora" do
 
     subject(:calc) { described_class.new() }
+
+    context '#div' do
+        it 'divide by 0' do
+            expect{subject.div(3,0)}.to raise_error(ZeroDivisionError)
+            expect{subject.div(3,0)}.to raise_error("divided by 0")
+            expect{subject.div(3,0)}.to raise_error(ZeroDivisionError, "divided by 0")
+            expect{subject.div(3,0)}.to raise_error(/divided/)
+        end
+    end
 
     context '#sum' do
         it 'positive numbers' do             
--- exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
+++ exemplo_rspec_tdd/spec/matchers/comparacao/comparacao_spec.rb
@@ -12,6 +12,7 @@ describe 'Matchers de Comparação' do
         expect(5).to be < 10 
     end
 
+    
     it '<=' do
         expect(5).to be <= 12
         expect(5).to be <= 5
