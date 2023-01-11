2c2fc0beb59a1fbfb3fe7ea6e779306d16447285
Gabriel <bieldeluna@gmail.com>
2023-01-11 11:50:57 -0300

    aula sobre fixtures

--- test_app/spec/models/customer_spec.rb
+++ test_app/spec/models/customer_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  fixtures :all # Carrega os dados no arquivo customers.yml
+
+  it 'Create a Customer' do
+    customer = customers(:gabriel)
+    expect(customer.full_name).to eq("Sr. Gabriel Piovesan")
+  end
 end
