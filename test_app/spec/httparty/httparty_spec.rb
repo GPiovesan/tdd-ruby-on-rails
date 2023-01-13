720cb65c9a9752dd3d6adef258d4da51845ee06b
Gabriel <bieldeluna@gmail.com>
2023-01-13 02:17:00 -0300

    VCR e urls

--- test_app/spec/httparty/httparty_spec.rb
+++ test_app/spec/httparty/httparty_spec.rb
@@ -1,13 +1,11 @@
 describe 'HTTParty' do
-    it 'content_type' do
+    it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do # Usando url não deterministica (url's diferentes)'match_requests_on'
         # Webmock - stub
         # criando dados falsos para teste
         #stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
         #    to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
-        VCR.use_cassette("jsonplaceholder/posts") do
-            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-            content_type = response.headers['content-type']
-            expect(content_type).to match(/application\/json/)
-        end
+        response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
+        content_type = response.headers['content-type']
+        expect(content_type).to match(/application\/json/)
     end
 end
