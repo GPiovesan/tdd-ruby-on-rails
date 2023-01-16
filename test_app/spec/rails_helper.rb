74ca980dbf877bd34b0ab0e6c9fda5375c5100ce
GPiovesan <bieldeluna@gmail.com>
2023-01-16 17:07:40 -0300

    Aula Shoulda Matchers

--- test_app/spec/rails_helper.rb
+++ test_app/spec/rails_helper.rb
@@ -34,6 +34,14 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
 
+  #Shoulda Matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
+  end
+
   # Time Heler
   config.include ActiveSupport::Testing::TimeHelpers
 
