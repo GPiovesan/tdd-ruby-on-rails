1e882a71896b67d84524a4856c74306742fb7c77
GPiovesan <bieldeluna@gmail.com>
2023-01-17 01:52:22 -0300

    Finalização da Seção 4 do curso

---
+++ test_app/spec/controllers/customers_controller_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+RSpec.describe CustomersController, type: :controller do
+
+    describe 'as a guest' do
+        context '#index' do
+            # it 'responds successfully' do
+            #   get :index
+            #   expect(response).to be_success
+            # end
+        
+            it 'returns a 200 response' do
+                get :index
+                expect(response).to have_http_status(200)
+            end
+        end
+        
+        context '#show' do
+            it 'returns a 302 response' do
+                customer = create(:customer)
+                get :show, params: { id: customer.id }
+                expect(response).to have_http_status(302)
+            end
+        end
+    end
+
+    describe 'As Logged Member' do
+        before do
+            @member = create(:member)
+            @customer = create(:customer)
+        end
+
+        it 'Content-Type JSON' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+            post :create, format: :json, params: { customer: customer_params }
+
+            expect(response.content_type).to eq('application/json; charset=utf-8')
+        end
+    
+        it 'Flash Notice' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+            post :create, params: { customer: customer_params }
+
+            expect(flash[:notice]).to match(/successfully created/)
+        end
+        
+        it 'with valid attributes' do
+            customer_params = attributes_for(:customer)
+            sign_in @member
+
+            expect {
+                post :create, params: { customer: customer_params }
+            }.to change(Customer, :count).by(1)
+        end
+
+        it 'responds a 200 response' do
+            sign_in @member
+            get :show, params: { id: @customer.id }
+            expect(response).to have_http_status(200)
+        end 
+
+        it 'render a :show template' do
+            sign_in @member
+            get :show, params: { id: @customer.id }
+            expect(response).to render_template(:show)
+        end 
+    end
+end
