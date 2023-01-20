c9eff6156e61ea97531607dbe76a52662843654a
GPiovesan <bieldeluna@gmail.com>
2023-01-20 04:17:14 -0300

    Finalização da Seção 6

--- test_app/spec/spec_helper.rb
+++ test_app/spec/spec_helper.rb
@@ -4,6 +4,9 @@ require 'HTTParty'
 require 'vcr'
 require 'capybara'
 
+# JSON Schema
+require 'json_matchers/rspec'
+
 VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
