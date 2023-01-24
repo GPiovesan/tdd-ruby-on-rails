4e1e6cd2e7b14c091d918e94e778123b74c02b68
GPiovesan <bieldeluna@gmail.com>
2023-01-23 23:20:05 -0300

    Criação de app crud com tdd clássico

--- tdd_app/spec/features/customer_spec.rb
+++ tdd_app/spec/features/customer_spec.rb
@@ -23,7 +23,7 @@ feature "Customers", type: :feature do
     visit(new_customer_path)
     customer_name = Faker::Name.name
 
-    fill_in('customer_name', with: customer_name)
+    fill_in('Nome', with: customer_name)
     fill_in('Email', with: Faker::Internet.email)
     fill_in('Telefone', with: Faker::PhoneNumber.phone_number)
     attach_file('Foto do Perfil', "#{Rails.root}/spec/fixtures/avatar.png")
@@ -32,5 +32,93 @@ feature "Customers", type: :feature do
 
     expect(page).to have_content('Cliente cadastrado com sucesso!')
     expect(Customer.last.name).to eq(customer_name)
+  end
+
+  scenario 'Não permite cadastrar um cliente inválido' do
+    visit(new_customer_path)
+    click_on('Criar Cliente')
+    expect(page).to have_content('não pode ficar em branco')
+  end
+
+  scenario 'Mostra um cliente' do
+    customer = Customer.create!(
+      name:  Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customer_path(customer.id))
+    expect(page).to have_content(customer.name)
+    expect(page).to have_content(customer.email)
+    expect(page).to have_content(customer.phone)
+  end
+
+  scenario 'Testando a Index' do
+    customer1 = Customer.create!(
+      name:  Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    customer2 = Customer.create!(
+      name:  Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    expect(page).to have_content(customer1.name).and have_content(customer2.name)
+  end
+
+  scenario 'Atualiza um Cliente' do
+    customer = Customer.create!(
+      name:  Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    new_name = Faker::Name.name
+    visit(edit_customer_path(customer.id))
+    fill_in('Nome', with: new_name)
+    click_on('Atualizar Cliente')
+
+    expect(page).to have_content('Cliente atualizado com sucesso!')
+    expect(page).to have_content(new_name)
+  end
+
+  scenario 'Click no link Mostrar' do
+    customer = Customer.create!(
+      name:  Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[2]/a").click
+    expect(page).to have_content("Mostrando Cliente")
+  end
+
+  scenario 'Click no link Editar na Index' do
+    customer = Customer.create!(
+      name:  Faker::Name.name,
+      email: Faker::Internet.email,
+      phone: Faker::PhoneNumber.phone_number,
+      smoker: ['S','N'].sample,
+      avatar: "#{Rails.root}/spec/fixtures/avatar.png"
+    )
+
+    visit(customers_path)
+    find(:xpath, "/html/body/table/tbody/tr[1]/td[3]/a").click
+    expect(page).to have_content("Editando Cliente")
   end
 end
